75e93e9f842cd4da697a7817a3e9307113cdcc97
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-03-09 20:09:59 -0300

    wip

--- src/main/java/br/com/dbc/vemser/trabalhofinal/dto/MedicoDTO.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/dto/MedicoDTO.java
@@ -1,5 +1,6 @@
 package br.com.dbc.vemser.trabalhofinal.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MedicoDTO extends MedicoCreateDTO{
+
+    @Schema(description = "Id do Medico", example = "1", required = true)
     private Integer idMedico;
+
+    @Schema(description = "Objeto usuarioDTO", required = true)
     private UsuarioDTO usuarioDTO;
+
+    @Schema(description = "Objeto especialidadeDTO", required = true)
     private EspecialidadeDTO especialidadeDTO;
 }
