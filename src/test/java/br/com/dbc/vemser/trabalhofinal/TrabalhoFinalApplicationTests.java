a480b4f131ce58352f5f09a9cb2d21df2dc522a0
Gabriel-Gomes-Meira <gabrielgomesm.contato@gmail.com>
2023-03-12 19:06:21 -0300

    Resolve Merge

---
+++ src/test/java/br/com/dbc/vemser/trabalhofinal/TrabalhoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vemser.trabalhofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
