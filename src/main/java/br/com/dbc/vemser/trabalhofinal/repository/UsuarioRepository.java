aefd4535b877780660f29bc0fdc2158cac65f3f5
Pedro <pedro.programmer.20@gmail.com>
2023-03-17 14:26:52 -0300

    UsuarioService em processo

--- src/main/java/br/com/dbc/vemser/trabalhofinal/repository/UsuarioRepository.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/repository/UsuarioRepository.java
@@ -4,8 +4,6 @@ import br.com.dbc.vemser.trabalhofinal.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
 
