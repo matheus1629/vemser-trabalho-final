03b091e936cc5c7e859bf87d1b7d0dbcaf6ed006
Thassio <thassio.vagner1@gmail.com>
2023-03-18 15:33:37 -0300

    Criando Query ClienteRepository

--- src/main/java/br/com/dbc/vemser/trabalhofinal/dto/ClientePersonalizadoDTO.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/dto/ClientePersonalizadoDTO.java
@@ -1,24 +1,29 @@
 package br.com.dbc.vemser.trabalhofinal.dto;
 
 import br.com.dbc.vemser.trabalhofinal.entity.TipoUsuario;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientePersonalizadoDTO {
 
     private Integer idCliente;
     private Integer idConvenio;
     private Integer idUsuario;
     private String cadastroOrgaoRegulador;
-    private Integer taxaAbatimento;
-    private Integer cpf;
+    private Double taxaAbatimento;
+    private String cpf;
     private String email;
     private String nome;
     private TipoUsuario tipoUsuario;
     private String contatos;
     private String cep;
     private Integer numero;
-    private List<AgendamentoDTO> agendamentoDTOList;
+//    private Set<AgendamentoDTO> agendamentoDTOList;
 }
--- src/main/java/br/com/dbc/vemser/trabalhofinal/repository/ClienteRepository.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/repository/ClienteRepository.java
@@ -1,11 +1,35 @@
 package br.com.dbc.vemser.trabalhofinal.repository;
 
+import br.com.dbc.vemser.trabalhofinal.dto.ClientePersonalizadoDTO;
 import br.com.dbc.vemser.trabalhofinal.entity.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteEntity, Integer> {
+
+    @Query("select new br.com.dbc.vemser.trabalhofinal.dto.ClientePersonalizadoDTO(" +
+            "c.idCliente," +
+            "c.idConvenio," +
+            "c.idUsuario," +
+            "con.cadastroOrgaoRegulador," +
+            "con.taxaAbatimento," +
+            "u.cpf," +
+            "u.email," +
+            "u.nome," +
+            "u.tipoUsuario," +
+            "u.contatos," +
+            "u.cep," +
+            "u.numero)" +
+            "from Cliente c" +
+            " left join c.usuarioEntity u " +
+            " left join c.convenioEntity con " +
+            "where c.idCliente = :id"
+    )
+    ClientePersonalizadoDTO clientePersonalizado(Integer id);
 
 
 }
