3218152a437edc10c70dbfdcbb9d88b311fc4d32
Pedro <pedro.programmer.20@gmail.com>
2023-03-19 12:17:36 -0300

    conflito

--- src/main/java/br/com/dbc/vemser/trabalhofinal/dto/AgendamentoDTO.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/dto/AgendamentoDTO.java
@@ -14,21 +14,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class AgendamentoDTO {
 
-    @Schema(description = "Id do agendamento")
+
+    //@Schema(description = "Nome do cliente")
+    //private String nome;
     private Integer idAgendamento;
-    @Schema(description = "Nome do cliente")
-    private String nomeCliente;
-    @Schema(description = "Nome do Médico")
-    private String nomeMedico;
-    @Schema(description = "Id do cliente", example = "1", required = true)
     private Integer idCliente;
-    @Schema(description = "Id do médico", example = "2", required = true)
     private Integer idMedico;
-    @Schema(description = "Tratamento a ser seguido pelo cliente", example = "Dipirona de 6 em 6 horas")
     private String tratamento;
-    @Schema(description = "Exame(s) pedidos pelo médico", example = "Sangue e urina")
     private String exame;
-    @Schema(description = "Data e horário da consulta", example = "25/08/2023 15:00", required = true)
     private LocalDateTime dataHorario;
 
 
