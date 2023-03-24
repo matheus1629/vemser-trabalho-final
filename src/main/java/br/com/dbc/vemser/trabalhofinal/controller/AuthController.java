f683db5984276e873f976b959f6ccb17977d126a
Thassio <thassio.vagner1@gmail.com>
2023-03-24 17:52:17 -0300

    criacao auth cliente e medico

--- src/main/java/br/com/dbc/vemser/trabalhofinal/controller/AuthController.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/controller/AuthController.java
@@ -1,8 +1,12 @@
 package br.com.dbc.vemser.trabalhofinal.controller;
 
-import br.com.dbc.vemser.trabalhofinal.dto.LoginDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.*;
+import br.com.dbc.vemser.trabalhofinal.entity.ClienteEntity;
+import br.com.dbc.vemser.trabalhofinal.entity.UsuarioEntity;
 import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.trabalhofinal.security.TokenService;
+import br.com.dbc.vemser.trabalhofinal.service.ClienteService;
+import br.com.dbc.vemser.trabalhofinal.service.MedicoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +23,20 @@ import javax.validation.Valid;
 public class AuthController {
 
     private final TokenService tokenService;
+    private final ClienteService clienteService;
+    private final MedicoService medicoService;
 
     @PostMapping
     public String auth(@RequestBody @Valid LoginDTO loginDTO) throws RegraDeNegocioException {
         return tokenService.autenticar(loginDTO);
     }
+    @PostMapping("/cadastro-cliente")
+    public ClienteCompletoDTO adicionarCliente(@RequestBody @Valid ClienteCreateDTO cliente) throws RegraDeNegocioException {
+        return clienteService.adicionar(cliente);
+    }
+    @PostMapping("/cadastro-medico")
+    public MedicoCompletoDTO adicionarMedico(@RequestBody @Valid MedicoCreateDTO medico) throws RegraDeNegocioException {
+        return medicoService.adicionar(medico);
+    }
+
 }
