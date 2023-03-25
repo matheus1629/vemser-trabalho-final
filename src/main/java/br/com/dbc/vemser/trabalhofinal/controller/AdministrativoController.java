8836770cd520467a3dab1ec2777f17593dca997c
Thassio <thassio.vagner1@gmail.com>
2023-03-25 11:59:51 -0300

    adicionando crud de adm

--- src/main/java/br/com/dbc/vemser/trabalhofinal/controller/AdministrativoController.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/controller/AdministrativoController.java
@@ -1,8 +1,11 @@
 package br.com.dbc.vemser.trabalhofinal.controller;
 
-import br.com.dbc.vemser.trabalhofinal.dto.UsuarioDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.*;
 import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.trabalhofinal.service.AdministrativoService;
+import br.com.dbc.vemser.trabalhofinal.service.ClienteService;
+import br.com.dbc.vemser.trabalhofinal.service.MedicoService;
+import br.com.dbc.vemser.trabalhofinal.service.UsuarioService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,8 +22,52 @@ public class AdministrativoController {
 
     private final AdministrativoService administrativoService;
 
+    private final ClienteService clienteService;
+    private final UsuarioService usuarioService;
+    private final MedicoService medicoService;
+
     @PutMapping("/{idUsuario}/reativação")
     public ResponseEntity<UsuarioDTO> reativarUsuario (@PathVariable ("idUsuario") Integer idUsuario) throws RegraDeNegocioException {
         return new ResponseEntity<>(administrativoService.reativarUsuario(idUsuario), HttpStatus.OK);
+    }
+
+    @GetMapping("/cliente/{id}")
+    public ResponseEntity<ClienteCompletoDTO> getByIdCliente(Integer id) throws RegraDeNegocioException {
+        return new ResponseEntity<>(clienteService.getById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/medico/{id}")
+    public ResponseEntity<MedicoCompletoDTO> getByIdMedico(Integer id) throws RegraDeNegocioException {
+        return new ResponseEntity<>(medicoService.getById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deletar-medico/{id}")
+    public ResponseEntity<Void> deleteMedico(Integer id) throws RegraDeNegocioException {
+        medicoService.remover(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/deletar-cliente/{id}")
+    public ResponseEntity<Void> deleteCliente(Integer id) throws RegraDeNegocioException {
+        clienteService.remover(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/criar-admin/{id}")
+    public ResponseEntity<UsuarioDTO> create(UsuarioCreateDTO usuario) throws RegraDeNegocioException {
+        return new ResponseEntity<>(administrativoService.adicionar(usuario), HttpStatus.OK);
+    }
+
+    @PutMapping("/editar-admin/{id}")
+    public ResponseEntity<UsuarioDTO> update(Integer id, UsuarioCreateDTO admin) throws RegraDeNegocioException {
+        UsuarioDTO usuarioAtualizado = administrativoService.editar(id,admin);
+        return new ResponseEntity<>(usuarioAtualizado, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/deletar-admin/{id}")
+    public ResponseEntity<Void> deleteAdmin(Integer id) throws RegraDeNegocioException {
+        administrativoService.remover(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/br/com/dbc/vemser/trabalhofinal/service/AdministrativoService.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/service/AdministrativoService.java
@@ -1,19 +1,67 @@
 package br.com.dbc.vemser.trabalhofinal.service;
 
+import br.com.dbc.vemser.trabalhofinal.dto.UsuarioCreateDTO;
 import br.com.dbc.vemser.trabalhofinal.dto.UsuarioDTO;
+import br.com.dbc.vemser.trabalhofinal.entity.UsuarioEntity;
 import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.trabalhofinal.repository.UsuarioRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import freemarker.template.TemplateException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Service
 public class AdministrativoService {
 
     private final UsuarioService usuarioService;
+    private final ObjectMapper objectMapper;
+    private final UsuarioRepository usuarioRepository;
 
+    private final EmailService emailService;
 
     public UsuarioDTO reativarUsuario(Integer idUsuario) throws RegraDeNegocioException {
         return usuarioService.reativarUsuario(idUsuario);
+    }
+
+    public UsuarioDTO adicionar(UsuarioCreateDTO usuario) throws RegraDeNegocioException{
+
+        UsuarioEntity usuarioEntity = objectMapper.convertValue(usuario, UsuarioEntity.class);
+        usuarioEntity.setIdCargo(1);
+
+        usuarioService.validarUsuarioAdicionado(usuarioEntity);
+        usuarioService.adicionar(usuarioEntity);
+
+        usuarioRepository.save(usuarioEntity);
+
+        return usuarioService.getById(usuarioEntity.getIdUsuario());
+    }
+
+    public UsuarioDTO editar(Integer id, UsuarioCreateDTO usuario) throws RegraDeNegocioException {
+
+        UsuarioEntity usuarioEntity = getAdm(id);
+
+        UsuarioCreateDTO usuarioCreateDTO = objectMapper.convertValue(usuario, UsuarioCreateDTO.class);
+        usuarioCreateDTO.setIdCargo(1);
+
+        usuarioService.validarUsuarioEditado(usuarioCreateDTO, usuarioEntity.getIdUsuario());
+        usuarioService.editar(usuarioCreateDTO, usuarioEntity.getIdUsuario());
+
+
+        UsuarioEntity usuarioEditado = usuarioRepository.save(usuarioEntity);
+
+        return usuarioService.getById(usuarioEditado.getIdUsuario());
+    }
+
+    public void remover(Integer id) throws RegraDeNegocioException {
+        usuarioService.remover(getAdm(id).getIdUsuario());
+    }
+
+    public UsuarioEntity getAdm(Integer id) throws RegraDeNegocioException {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new RegraDeNegocioException("Administrador não existe!"));
     }
 }
