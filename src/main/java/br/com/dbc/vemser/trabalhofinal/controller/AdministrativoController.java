00ffd3c4f882fccbeb95f3c527347bbec211f681
Pedro <pedro.programmer.20@gmail.com>
2023-03-24 21:26:47 -0300

    Email service testado

--- src/main/java/br/com/dbc/vemser/trabalhofinal/controller/AdministrativoController.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/controller/AdministrativoController.java
@@ -3,12 +3,14 @@ package br.com.dbc.vemser.trabalhofinal.controller;
 import br.com.dbc.vemser.trabalhofinal.dto.UsuarioDTO;
 import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.trabalhofinal.service.AdministrativoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name="Administrativo")
 @Validated
 @RestController
 @RequestMapping("/administrativo")
