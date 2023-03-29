4de7ac06ac8fa2125a781748b5f2bce93ef3004c
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-03-29 17:20:40 -0300

    test clases

---
+++ src/test/java/br/com/dbc/vemser/trabalhofinal/service/ConvenioServiceTest.java
@@ -0,0 +1,6 @@
+package br.com.dbc.vemser.trabalhofinal.service;
+
+
+public class ConvenioServiceTest {
+
+}
---
+++ src/test/java/br/com/dbc/vemser/trabalhofinal/service/UsuarioServiceTest.java
@@ -0,0 +1,7 @@
+package br.com.dbc.vemser.trabalhofinal.service;
+
+
+public class UsuarioServiceTest {
+
+
+}
