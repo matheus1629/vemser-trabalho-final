342fda47c00f9518594bb8c7ae4a59bf104cc9e9
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-03-29 17:03:51 -0300

    classe test

---
+++ src/test/java/br/com/dbc/vemser/trabalhofinal/service/AdministrativoService.java
@@ -0,0 +1,158 @@
+package br.com.dbc.vemser.trabalhofinal.service;
+
+import br.com.dbc.vemser.trabalhofinal.client.EnderecoClient;
+import br.com.dbc.vemser.trabalhofinal.dto.PageDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.cliente.ClienteCompletoDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.medico.MedicoCompletoDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.usuario.UsuarioCreateDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.usuario.UsuarioDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.usuario.UsuarioUpdateDTO;
+import br.com.dbc.vemser.trabalhofinal.entity.UsuarioEntity;
+import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
+import br.com.dbc.vemser.trabalhofinal.repository.ClienteRepository;
+import br.com.dbc.vemser.trabalhofinal.repository.MedicoRepository;
+import br.com.dbc.vemser.trabalhofinal.repository.UsuarioRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import freemarker.template.TemplateException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class AdministrativoService {
+
+    private final UsuarioService usuarioService;
+    private final ObjectMapper objectMapper;
+    private final UsuarioRepository usuarioRepository;
+    private final EmailService emailService;
+    private final ClienteService clienteService;
+    private final MedicoService medicoService;
+    private final MedicoRepository medicoRepository;
+    private final ClienteRepository clienteRepository;
+    private final EnderecoClient enderecoClient;
+
+    public UsuarioDTO reativarUsuario(Integer idUsuario) throws RegraDeNegocioException {
+        return usuarioService.reativarUsuario(idUsuario);
+    }
+
+    public List<UsuarioDTO> listar(){
+        List<UsuarioDTO> usuarioDTOS = usuarioRepository.findAll()
+                .stream()
+                .filter(usuarioEntity -> usuarioEntity.getIdCargo().equals(1))
+                .filter(usuario -> usuario.getAtivo().equals(1))
+                .map(adm -> objectMapper.convertValue(adm, UsuarioDTO.class))
+                .collect(Collectors.toList());
+        System.out.println();
+        for (UsuarioDTO usuarioDTO: usuarioDTOS) {
+            usuarioDTO.setEnderecoDTO(enderecoClient.getEndereco(usuarioDTO.getCep()));
+        }
+
+        return usuarioDTOS;
+    }
+
+    public UsuarioDTO adicionar(UsuarioCreateDTO usuario) throws RegraDeNegocioException{
+
+        UsuarioEntity usuarioEntity = objectMapper.convertValue(usuario, UsuarioEntity.class);
+        usuarioEntity.setIdCargo(1);
+
+        usuarioService.validarUsuarioAdicionado(usuarioEntity);
+        usuarioService.adicionar(usuarioEntity);
+
+        usuarioRepository.save(usuarioEntity);
+
+        try{
+            emailService.sendEmailUsuario(usuarioEntity, TipoEmail.USUARIO_CADASTRO, null);
+        } catch (MessagingException | TemplateException | IOException e) {
+            usuarioService.hardDelete(usuarioEntity.getIdUsuario());
+            throw new RegraDeNegocioException("Erro ao enviar o e-mail. Cadastro não realizado.");
+        }
+
+        return usuarioService.getById(usuarioEntity.getIdUsuario());
+    }
+
+    public UsuarioDTO editar(Integer id, UsuarioUpdateDTO usuario) throws RegraDeNegocioException {
+
+        UsuarioEntity usuarioEntity = getAdm(id);
+        if (!usuarioEntity.getIdCargo().equals(1)) {
+            throw new RegraDeNegocioException("Este usuário não é um administrador.");
+        }
+
+        UsuarioCreateDTO usuarioCreateDTO = objectMapper.convertValue(usuario, UsuarioCreateDTO.class);
+
+        usuarioService.validarUsuarioEditado(usuarioCreateDTO, usuarioEntity.getIdUsuario());
+        usuarioService.editar(usuarioCreateDTO, usuarioEntity.getIdUsuario());
+
+        UsuarioEntity usuarioEditado = usuarioRepository.save(usuarioEntity);
+
+        return usuarioService.getById(usuarioEditado.getIdUsuario());
+    }
+
+    public void remover(Integer id) throws RegraDeNegocioException {
+        usuarioService.remover(getAdm(id).getIdUsuario());
+    }
+
+    public UsuarioEntity getAdm(Integer id) throws RegraDeNegocioException {
+        return usuarioRepository.findById(id)
+                .filter(usuario -> usuario.getAtivo().equals(1))
+                .orElseThrow(() -> new RegraDeNegocioException("Administrador não existe!"));
+    }
+
+    public ClienteCompletoDTO getClienteById(Integer id) throws RegraDeNegocioException {
+        return clienteService.getById(id);
+    }
+
+    public MedicoCompletoDTO getMedicoById(Integer id) throws RegraDeNegocioException {
+        return medicoService.getById(id);
+    }
+
+    public void removerMedico(Integer id) throws RegraDeNegocioException {
+        medicoService.remover(id);
+    }
+
+    public void removerCliente(Integer id) throws RegraDeNegocioException {
+        clienteService.remover(id);
+    }
+
+    public PageDTO<MedicoCompletoDTO> listMedico(Integer pagina, Integer tamanho){
+        Pageable solicitacaoPagina = PageRequest.of(pagina,tamanho);
+        Page<MedicoCompletoDTO> medico = medicoRepository.listarFull(solicitacaoPagina);
+        List<MedicoCompletoDTO> medicoCompletoDTOS = medico.getContent().stream()
+                .toList();
+
+        for (MedicoCompletoDTO medicoCompletoDTO: medicoCompletoDTOS) {
+            medicoCompletoDTO.setEnderecoDTO(enderecoClient.getEndereco(medicoCompletoDTO.getCep()));
+        }
+
+        return new PageDTO<>(medico.getTotalElements(),
+                medico.getTotalPages(),
+                pagina,
+                tamanho,
+                medicoCompletoDTOS);
+    }
+
+    public PageDTO<ClienteCompletoDTO> listCliente(Integer pagina, Integer tamanho) {
+        Pageable solicitacaoPagina = PageRequest.of(pagina,tamanho);
+        Page<ClienteCompletoDTO> cliente = clienteRepository.listarFull(solicitacaoPagina);
+        List<ClienteCompletoDTO> clienteCompletoDTOS = cliente.getContent().stream()
+                .toList();
+
+        for (ClienteCompletoDTO clienteCompletoDTO: clienteCompletoDTOS) {
+            clienteCompletoDTO.setEnderecoDTO(enderecoClient.getEndereco(clienteCompletoDTO.getCep()));
+        }
+
+        return new PageDTO<>(cliente.getTotalElements(),
+                cliente.getTotalPages(),
+                pagina,
+                tamanho,
+                clienteCompletoDTOS);
+    }
+}
---
+++ src/test/java/br/com/dbc/vemser/trabalhofinal/service/MedicoService.java
@@ -0,0 +1,150 @@
+package br.com.dbc.vemser.trabalhofinal.service;
+
+import br.com.dbc.vemser.trabalhofinal.client.EnderecoClient;
+import br.com.dbc.vemser.trabalhofinal.dto.agendamento.AgendamentoListaDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.agendamento.AgendamentoMedicoRelatorioDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.medico.MedicoCompletoDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.medico.MedicoCreateDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.medico.MedicoUpdateDTO;
+import br.com.dbc.vemser.trabalhofinal.dto.usuario.UsuarioCreateDTO;
+import br.com.dbc.vemser.trabalhofinal.entity.MedicoEntity;
+import br.com.dbc.vemser.trabalhofinal.entity.UsuarioEntity;
+import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
+import br.com.dbc.vemser.trabalhofinal.repository.MedicoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import freemarker.template.TemplateException;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MedicoService {
+    private final MedicoRepository medicoRepository;
+    private final ObjectMapper objectMapper;
+    private final UsuarioService usuarioService;
+    private final EspecialidadeService especialidadeService;
+    private final AgendamentoService agendamentoService;
+    private final EmailService emailService;
+    private final EnderecoClient enderecoClient;
+
+    public MedicoService(MedicoRepository medicoRepository,
+                         ObjectMapper objectMapper,
+                         UsuarioService usuarioService,
+                         EspecialidadeService especialidadeService,
+                         @Lazy AgendamentoService agendamentoService,
+                         EmailService emailService, EnderecoClient enderecoClient) {
+        this.medicoRepository = medicoRepository;
+        this.objectMapper = objectMapper;
+        this.usuarioService = usuarioService;
+        this.especialidadeService = especialidadeService;
+        this.agendamentoService = agendamentoService;
+        this.emailService = emailService;
+        this.enderecoClient = enderecoClient;
+    }
+
+
+
+    public MedicoCompletoDTO recuperarMedico() throws RegraDeNegocioException {
+        MedicoEntity medicoEntity = medicoRepository.getMedicoEntityByIdUsuario(usuarioService.getIdLoggedUser());
+        return getById(medicoEntity.getIdMedico());
+    }
+
+    public MedicoCompletoDTO getById(Integer idMedico) throws RegraDeNegocioException {
+        Optional<MedicoCompletoDTO> medicoRetornado = medicoRepository.getByIdPersonalizado(idMedico);
+        if (medicoRetornado.isEmpty()) {
+            throw new RegraDeNegocioException("Usuário não encontrado.");
+        }
+        medicoRetornado.get().setEnderecoDTO(enderecoClient.getEndereco(medicoRetornado.get().getCep()));
+        return medicoRetornado.get();
+    }
+
+    public MedicoEntity getMedico(Integer id) throws RegraDeNegocioException {
+        return medicoRepository.findById(id)
+                .filter(medicoEntity -> medicoEntity.getUsuarioEntity().getAtivo().equals(1))
+                .orElseThrow(() -> new RegraDeNegocioException("Médico não existe!"));
+    }
+
+    public AgendamentoListaDTO getMedicoAgentamentos() throws RegraDeNegocioException {
+        MedicoCompletoDTO medicoCompletoDTO = recuperarMedico();
+        AgendamentoMedicoRelatorioDTO agendamentoMedicoRelatorioDTO = agendamentoService.getRelatorioMedicoById(medicoCompletoDTO.getIdMedico());
+
+        return objectMapper.convertValue(agendamentoMedicoRelatorioDTO, AgendamentoListaDTO.class);
+    }
+
+    public MedicoCompletoDTO adicionar(MedicoCreateDTO medico) throws RegraDeNegocioException {
+        checarSeTemNumero(medico.getNome());
+
+        UsuarioEntity usuarioEntity = objectMapper.convertValue(medico, UsuarioEntity.class);
+        usuarioEntity.setIdCargo(2);
+
+        // Adicionando o usuario que foi salvado no Medico a salvar
+        MedicoEntity medicoEntity = objectMapper.convertValue(medico, MedicoEntity.class);
+        usuarioEntity.setMedicoEntity(medicoEntity);
+        medicoEntity.setUsuarioEntity(usuarioEntity);
+
+        // Adicionando Convenio em Medico a salvar
+        medicoEntity.setEspecialidadeEntity(especialidadeService.getEspecialidade(medico.getIdEspecialidade()));
+
+        usuarioService.validarUsuarioAdicionado(usuarioEntity);
+        usuarioService.adicionar(usuarioEntity);
+
+        medicoRepository.save(medicoEntity);
+        try {
+            emailService.sendEmailUsuario(medicoEntity.getUsuarioEntity(), TipoEmail.USUARIO_CADASTRO, null);
+        } catch (MessagingException | TemplateException | IOException e) {
+            usuarioService.hardDelete(medicoEntity.getUsuarioEntity().getIdUsuario());
+            throw new RegraDeNegocioException("Erro ao enviar o e-mail. Cadastro não realizado.");
+        }
+
+        return getById(medicoEntity.getIdMedico());
+    }
+
+    public MedicoCompletoDTO editar(MedicoUpdateDTO medico) throws RegraDeNegocioException {
+        MedicoEntity medicoEntity = objectMapper.convertValue(recuperarMedico(), MedicoEntity.class);
+
+        medicoEntity.setCrm(medico.getCrm());
+        medicoEntity.setEspecialidadeEntity(especialidadeService.getEspecialidade(medico.getIdEspecialidade()));
+        medicoEntity.setUsuarioEntity(usuarioService.getUsuario(medicoEntity.getIdUsuario()));
+
+        checarSeTemNumero(medico.getNome());
+
+        UsuarioCreateDTO usuarioCreateDTO = objectMapper.convertValue(medico, UsuarioCreateDTO.class);
+
+        List<MedicoEntity> listaMedico = medicoRepository.findAll().stream()
+                .filter(medicoEntity1 -> !medicoEntity1.getCrm().equals(medico.getCrm()))
+                .toList();
+
+        for (MedicoEntity medicoVerificarCRM : listaMedico) {
+            if (medico.getCrm().equals(medicoVerificarCRM.getCrm())) {
+                throw new RegraDeNegocioException("CRM já existe!");
+            }
+        }
+
+        usuarioService.validarUsuarioEditado(usuarioCreateDTO, medicoEntity.getIdUsuario());
+        usuarioService.editar(usuarioCreateDTO, medicoEntity.getIdUsuario());
+
+        MedicoEntity medicoEditado = medicoRepository.save(medicoEntity);
+
+        return getById(medicoEditado.getIdMedico());
+    }
+
+    @Transactional
+    public void remover(Integer id) throws RegraDeNegocioException {
+        MedicoEntity medicoEntity = getMedico(id);
+        usuarioService.remover(medicoEntity.getIdUsuario());
+        agendamentoService.removerPorMedicoDesativado(medicoEntity);
+    }
+
+    public void checarSeTemNumero(String string) throws RegraDeNegocioException {
+        if (string.matches(".*[0-9].*")) { // checa se tem número no nome
+            throw new RegraDeNegocioException("O nome da especialidade não pode conter número");
+        }
+    }
+
+
+}
