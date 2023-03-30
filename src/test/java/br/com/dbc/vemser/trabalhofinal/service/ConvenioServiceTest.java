cb1b56ff75c3f04f430a8d506a19bc8d9405ed83
Thassio <thassio.vagner1@gmail.com>
2023-03-29 22:53:10 -0300

    teste na MedicoServiceTest

--- src/test/java/br/com/dbc/vemser/trabalhofinal/service/ConvenioServiceTest.java
+++ src/test/java/br/com/dbc/vemser/trabalhofinal/service/ConvenioServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.dbc.vemser.trabalhofinal.service;
 
+import br.com.dbc.vemser.trabalhofinal.dto.PageDTO;
 import br.com.dbc.vemser.trabalhofinal.dto.convenio.ConvenioCreateDTO;
 import br.com.dbc.vemser.trabalhofinal.dto.convenio.ConvenioDTO;
 import br.com.dbc.vemser.trabalhofinal.entity.ConvenioEntity;
@@ -14,20 +15,30 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.junit.Before;
 
 import javax.validation.constraints.NotNull;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConvenioServiceTest {
+    @Spy
     @InjectMocks
     private ConvenioService convenioService;
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -62,40 +73,58 @@ public class ConvenioServiceTest {
         assertEquals(convenioMockadaDoBanco.getCadastroOrgaoRegulador(), convenioRetornado.getCadastroOrgaoRegulador());
         assertEquals(1,convenioRetornado.getIdConvenio());
     }
+    @Test
+    public void deveListarComSucesso(){
+        // SETUP
+        List<ConvenioEntity> lista = List.of(getConvenioEntityMock(), getConvenioEntityMock(), getConvenioEntityMock());
+        when(convenioRepository.findAll()).thenReturn(lista);
 
-//    public ConvenioDTO getById(Integer id) throws RegraDeNegocioException {
-//        return objectMapper.convertValue(getConvenio(id), ConvenioDTO.class);
-//    }
+        // ACT
+        PageDTO<ConvenioDTO> list = convenioService.list(1,1);
+
+        // ASSERT
+        assertNotNull(list);
+        assertEquals(3, 3);
+    }
+//    public PageDTO<ConvenioDTO> list(Integer pagina, Integer tamanho) {
+//        Pageable solicitacaoPagina = PageRequest.of(pagina,tamanho);
+//        Page<ConvenioEntity> convenio = convenioRepository.findAll(solicitacaoPagina);
+//        List<ConvenioDTO> convenioDTO = convenio.getContent().stream()
+//                .map(x -> objectMapper.convertValue(x, ConvenioDTO.class))
+//                .toList();
 //
-//    public ConvenioDTO adicionar(ConvenioCreateDTO convenio) throws RegraDeNegocioException {
-//        ConvenioEntity convenioEntity = objectMapper.convertValue(convenio, ConvenioEntity.class);
-//        ConvenioEntity convenioEntityAdicionado = convenioRepository.save(convenioEntity);
-//        return objectMapper.convertValue(convenioEntityAdicionado, ConvenioDTO.class);
-//    }
-//
-//    public void remover(Integer id) throws RegraDeNegocioException {
-//        getConvenio(id);
-//        convenioRepository.deleteById(id);
-//    }
-//
-//
-//    public ConvenioDTO editar(Integer id, ConvenioCreateDTO convenio) throws RegraDeNegocioException {
-//        ConvenioEntity convenioEntityRecuperado = getConvenio(id);
-//        convenioEntityRecuperado.setTaxaAbatimento(convenio.getTaxaAbatimento());
-//        convenioEntityRecuperado.setCadastroOrgaoRegulador(convenio.getCadastroOrgaoRegulador());
-//
-//        convenioRepository.save(convenioEntityRecuperado);
-//
-//        return objectMapper.convertValue(convenioEntityRecuperado, ConvenioDTO.class);
-//    }
-//
-//    public ConvenioEntity getConvenio(Integer id) throws RegraDeNegocioException {
-//        return convenioRepository.findById(id).orElseThrow(() -> new RegraDeNegocioException("Convenio não existe"));
-//    }
+//        return new PageDTO<>(convenio.getTotalElements(),
+//                convenio.getTotalPages(),
+//                pagina,
+//                tamanho,
+//                convenioDTO);
+
+    @Test
+    public void deveRetornarConvenioPorId() throws RegraDeNegocioException{
+        // declaração de variaveis (SETUP)
+        ConvenioEntity convenioMockadaDoBanco = getConvenioEntityMock();
+        ConvenioDTO convenioMockadaDoBancoDTO = getConvenioEntityMockDTO();
+        when(convenioRepository.findById(any())).thenReturn(Optional.of(convenioMockadaDoBanco));
+//        Mockito.doReturn(convenioMockadaDoBanco).when(convenioService).getConvenio(anyInt());
+        // ACT
+        ConvenioDTO resultado = convenioService.getById(1);
+        // Assert
+        assertNotNull(resultado);
+        assertEquals(convenioMockadaDoBancoDTO,resultado);
+    }
 
     @NotNull
     private static ConvenioEntity getConvenioEntityMock() {
         ConvenioEntity convenioMockadaDoBanco = new ConvenioEntity();
+        convenioMockadaDoBanco.setIdConvenio(1);
+        convenioMockadaDoBanco.setCadastroOrgaoRegulador("Exemplo A");
+        convenioMockadaDoBanco.setTaxaAbatimento(50.0);
+        return convenioMockadaDoBanco;
+    }
+
+    @NotNull
+    private static ConvenioDTO getConvenioEntityMockDTO() {
+        ConvenioDTO convenioMockadaDoBanco = new ConvenioDTO();
         convenioMockadaDoBanco.setIdConvenio(1);
         convenioMockadaDoBanco.setCadastroOrgaoRegulador("Exemplo A");
         convenioMockadaDoBanco.setTaxaAbatimento(50.0);
--- src/test/java/br/com/dbc/vemser/trabalhofinal/service/MedicoServiceTest.java
+++ src/test/java/br/com/dbc/vemser/trabalhofinal/service/MedicoServiceTest.java
@@ -1,7 +1,38 @@
 package br.com.dbc.vemser.trabalhofinal.service;
 
 
+import br.com.dbc.vemser.trabalhofinal.dto.convenio.ConvenioDTO;
+import br.com.dbc.vemser.trabalhofinal.entity.ConvenioEntity;
+import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MedicoServiceTest {
-
-
+    @Spy
+    @InjectMocks
+    private MedicoService medicoService;
+    @Test
+    public void deveRetornarConvenioPorId() throws RegraDeNegocioException {
+        // declaração de variaveis (SETUP)
+        ConvenioEntity convenioMockadaDoBanco = getConvenioEntityMock();
+        ConvenioDTO convenioMockadaDoBancoDTO = getConvenioEntityMockDTO();
+        when(convenioRepository.findById(any())).thenReturn(Optional.of(convenioMockadaDoBanco));
+//        Mockito.doReturn(convenioMockadaDoBanco).when(convenioService).getConvenio(anyInt());
+        // ACT
+        ConvenioDTO resultado = convenioService.getById(1);
+        // Assert
+        assertNotNull(resultado);
+        assertEquals(convenioMockadaDoBancoDTO,resultado);
+    }
 }
