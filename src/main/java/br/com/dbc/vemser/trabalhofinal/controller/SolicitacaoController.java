4994019b57d22be6eb05c6d63ae33d2f6ca6341d
Thassio <thassio.vagner1@gmail.com>
2023-04-02 15:54:42 -0300

    hotfix

--- src/main/java/br/com/dbc/vemser/trabalhofinal/controller/SolicitacaoController.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/controller/SolicitacaoController.java
@@ -32,19 +32,6 @@ public class SolicitacaoController {
     public ResponseEntity<SolicitacaoDTO> Requisitarsolicitacao(@RequestBody @Valid SolicitacaoCreateDTO solicitacaoCreateDTO) throws RegraDeNegocioException {
         return new ResponseEntity<>(solicitacaoService.create(solicitacaoCreateDTO), HttpStatus.OK);
     }
-
-<<<<<<< HEAD
-//    @GetMapping("/resgatar-personalizado/{idMedico}/{idCliente}/{dataHoraInicio}/{dataHoraFim}/{statusSolicitacao}")
-//    public ResponseEntity<List<SolicitacaoDTO>> resgatarSolicitacao(@RequestParam( value = "idMedico", required = false) Integer idMedico,
-//                                                                    @RequestParam(value = "idCliente", required = false) Integer idCliente,
-//                                                                    @RequestParam (value = "dataHoraInicio", required = false)
-//                                                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHoraInicio,
-//                                                                    @RequestParam(value = "dataHoraFim", required = false)
-//                                                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataHoraFim,
-//                                                                    @RequestParam(value = "statusSolicitacao", required = false)StatusSolicitacao statusSolicitacao) {
-//        return new ResponseEntity<>(solicitacaoService.findSolicitacoes(idMedico, idCliente, dataHoraInicio, dataHoraFim, statusSolicitacao), HttpStatus.OK);
-//    }
-=======
     @GetMapping("/resgatar-personalizado")
     public ResponseEntity<List<SolicitacaoDTO>> resgatarSolicitacao(@RequestParam( value = "idMedico", required = false) Integer idMedico,
                                                                     @RequestParam(value = "idCliente", required = false) Integer idCliente,
@@ -55,7 +42,6 @@ public class SolicitacaoController {
                                                                     @RequestParam(value = "statusSolicitacao", required = false)StatusSolicitacao statusSolicitacao) {
         return new ResponseEntity<>(solicitacaoService.findSolicitacoes(idMedico, idCliente, dataHoraInicio, dataHoraFim, statusSolicitacao), HttpStatus.OK);
     }
->>>>>>> 01e82e007ff926e09b598d612d2d82d09f613820
 
 
 }
--- src/main/java/br/com/dbc/vemser/trabalhofinal/service/SolicitacaoService.java
+++ src/main/java/br/com/dbc/vemser/trabalhofinal/service/SolicitacaoService.java
@@ -2,6 +2,7 @@ package br.com.dbc.vemser.trabalhofinal.service;
 
 import br.com.dbc.vemser.trabalhofinal.dto.solicitacao.SolicitacaoCreateDTO;
 import br.com.dbc.vemser.trabalhofinal.dto.solicitacao.SolicitacaoDTO;
+import br.com.dbc.vemser.trabalhofinal.entity.QSolicitacaoEntity;
 import br.com.dbc.vemser.trabalhofinal.entity.SolicitacaoEntity;
 import br.com.dbc.vemser.trabalhofinal.entity.StatusSolicitacao;
 import br.com.dbc.vemser.trabalhofinal.exceptions.RegraDeNegocioException;
@@ -39,40 +40,40 @@ public class SolicitacaoService {
 
     }
 
-//    public List<SolicitacaoDTO> findSolicitacoes(
-//            Integer idMedico, Integer idCliente, LocalDateTime dataHoraInicio, LocalDateTime
-//            dataHoraFim, StatusSolicitacao statusSolicitacao
-//    ) {
-//
-//        if (dataHoraInicio == null) {
-//            dataHoraInicio = (LocalDateTime.of(2000, 01, 01, 00, 00));
-//        }
-//
-//        if (dataHoraFim == null) {
-//            dataHoraFim = (LocalDateTime.of(3000, 01, 01, 00, 00));
-//        }
-//
-//        QSolicitacaoEntity solicitacao = QSolicitacaoEntity.solicitacaoEntity;
-//        BooleanBuilder builder = new BooleanBuilder();
-//
-//        if (idMedico != null) {
-//            builder.and(solicitacao.idMedico.eq(idMedico));
-//        }
-//
-//        if (idCliente != null) {
-//            builder.and(solicitacao.idCliente.eq(idCliente));
-//        }
-//
-//        if (statusSolicitacao != null) {
-//            builder.and(solicitacao.statusSolicitacao.eq(statusSolicitacao));
-//        }
-//
-//        builder.and(solicitacao.dataHora.between(dataHoraInicio, dataHoraFim));
-//
-//        List<SolicitacaoEntity> results = (List<SolicitacaoEntity>) solicitacaoReposiroty.findAll(builder.getValue());
-//
-//        return results.stream().map(solicitacaoEntity ->  objectMapper.convertValue(solicitacaoEntity, SolicitacaoDTO.class)).collect(Collectors.toList());
-//    }
+    public List<SolicitacaoDTO> findSolicitacoes(
+            Integer idMedico, Integer idCliente, LocalDateTime dataHoraInicio, LocalDateTime
+            dataHoraFim, StatusSolicitacao statusSolicitacao
+    ) {
+
+        if (dataHoraInicio == null) {
+            dataHoraInicio = (LocalDateTime.of(2000, 01, 01, 00, 00));
+        }
+
+        if (dataHoraFim == null) {
+            dataHoraFim = (LocalDateTime.of(3000, 01, 01, 00, 00));
+        }
+
+        QSolicitacaoEntity solicitacao = QSolicitacaoEntity.solicitacaoEntity;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (idMedico != null) {
+            builder.and(solicitacao.idMedico.eq(idMedico));
+        }
+
+        if (idCliente != null) {
+            builder.and(solicitacao.idCliente.eq(idCliente));
+        }
+
+        if (statusSolicitacao != null) {
+            builder.and(solicitacao.statusSolicitacao.eq(statusSolicitacao));
+        }
+
+        builder.and(solicitacao.dataHora.between(dataHoraInicio, dataHoraFim));
+
+        List<SolicitacaoEntity> results = (List<SolicitacaoEntity>) solicitacaoReposiroty.findAll(builder.getValue());
+
+        return results.stream().map(solicitacaoEntity ->  objectMapper.convertValue(solicitacaoEntity, SolicitacaoDTO.class)).collect(Collectors.toList());
+    }
 }
 
 
